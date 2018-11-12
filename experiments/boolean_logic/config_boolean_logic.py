91afd5c66384a5d0a46bb00f633c95c295df87ea
Bram de Wilde <brambo5@live.nl>
2018-11-13 00:55:25 +0100

    Added 4x4 grid and Boolean experiments

---
+++ experiments/boolean_logic/config_boolean_logic.py
@@ -0,0 +1,157 @@
+import numpy as np
+from SkyNEt.config.config_class import config_class
+
+class experiment_config(config_class):
+    '''This is the experiment configuration file for the boolean logic experiment.
+    It also serves as a template for other experiments, so please model your work
+    after this when you make new experiments.
+    This experiment_config class inherits from config_class default values that are known to work well with boolean logic.
+    You can define user-specific parameters in the construction of the object in __init__() or define
+    methods that you might need after, e.g. a new fitness function or input and output generators.
+    Remember if you define a new fitness function or generator, you have to redefine the self.Fitness,
+    self.Target_gen and self.Input_gen in __init__()
+
+    ----------------------------------------------------------------------------
+    Description of general parameters
+    ----------------------------------------------------------------------------
+    comport; the COM port to which the ivvi rack is connected.
+    amplification; specify the amount of nA/V. E.g. if you set the IVVI to 100M,
+        then amplification = 10
+    generations; the amount of generations for the GA
+    generange; the range that each gene ([0, 1]) is mapped to. E.g. in the Boolean
+        experiment the genes for the control voltages are mapped to the desired
+        control voltage range.
+    partition; this tells the GA how it will evolve the next generation.
+        In order, this will make the GA evolve the specified number with
+        - promoting fittest partition[0] genomes unchanged
+        - adding Gaussian noise to the fittest partition[1] genomes
+        - crossover between fittest partition[2] genomes
+        - crossover between fittest partition[3] and randomly selected genes
+        - randomly adding parition[4] genomes
+    genomes; the amount of genomes in the genepool, speficy this parameter instead
+        of partition if you don't care about the specific partition.
+    genes; the amount of genes per genome
+    mutationrate; the probability of mutation for each gene (between 0 and 1)
+    fitnessavg; the amount of times the same genome is tested to obtain the fitness
+        value.
+    fitnessparameters; the parameters for FitnessEvolution (see its doc for
+        specifics)
+    filepath; the path used for saving your experiment data
+    name; name used for experiment data file (date/time will be appended)
+
+    ----------------------------------------------------------------------------
+    Description of method parameters
+    ----------------------------------------------------------------------------
+    signallength; the length in s of the Boolean P and Q signals
+    edgelength; the length in s of the edge between 0 and 1 in P and Q
+    fs; sample frequency for niDAQ or ADwin
+
+    ----------------------------------------------------------------------------
+    Description of methods
+    ----------------------------------------------------------------------------
+    TargetGen; specify the target function you wish to evolve, options are:
+        - OR
+        - AND
+        - NOR
+        - NAND
+        - XOR
+        - XNOR
+    Fitness; specify the fitness function, standard options are:
+        - FitnessEvolution; standard fitness used for boolean logic
+        - FitnessNMSE; normalised mean squared error
+    '''
+
+    def __init__(self):
+        super().__init__() #DO NOT REMOVE!
+        ################################################
+        ######### SPECIFY PARAMETERS ###################
+        ################################################
+        # Define experiment
+        self.TargetGen = self.NOR
+        self.generations = 20
+        self.generange = [[0, 1]]*5
+        self.P = 0
+        self.Q = 1
+        self.output = 7
+        self.controls = [2, 3, 4, 5, 6]
+
+        # Specify either partition or genomes
+        #self.partition = [5, 5, 5, 5, 5]
+        self.genomes = 10
+
+        # Documentation
+        self.genelabels = ['CV1/T11','CV2/T13','CV3/T17','CV4/T7','CV5/T1', 'Input scaling']
+
+        # Save settings
+        self.filepath = r'D:\Data\Bram\evolution_test\\'  #Important: end path with double backslash
+        self.name = 'NOR'
+
+        ################################################
+        ################# OFF-LIMITS ###################
+        ################################################
+        # Check if genomes parameter has been changed
+        if(self.genomes != sum(self.default_partition)):
+            if(self.genomes%5 == 0):
+                self.partition = [int(self.genomes/5)]*5  # Construct equally partitioned genomes
+            else:
+                print('WARNING: The specified number of genomes is not divisible by 5.'
+                      + ' The remaining genomes are generated randomly each generation. '
+                      + ' Specify partition in the config instead of genomes if you do not want this.')
+                self.partition = [self.genomes//5]*5  # Construct equally partitioned genomes
+                self.partition[-1] += self.genomes%5  # Add remainder to last entry of partition
+
+        self.genomes = int(sum(self.partition))  # Make sure genomes parameter is correct
+        self.genes = int(len(self.generange))  # Make sure genes parameter is correct
+
+        self.Fitness = self.FitnessFix
+        self.fitnessparameters = [0, 0, 1, 0]
+
+    #####################################################
+    ############# USER-SPECIFIC METHODS #################
+    #####################################################
+    # Optionally define new methods here that you wish to use in your experiment.
+    # These can be e.g. new fitness functions or input/output generators.
+    def FitnessFix(self, x, target):
+        '''
+        This implements the fitness function
+        F = self.fitnessparameters[0] * m / (sqrt(r) + self.fitnessparameters[3] * abs(c)) + self.fitnessparameters[1] / r + self.fitnessparameters[2] * Q
+        where m,c,r follow from fitting x = m*target + c to minimize r
+        and Q is the fitness quality as defined by Celestine in his thesis
+        appendix 9
+        W is a weight array consisting of 0s and 1s with equal length to x and
+        target. Points where W = 0 are not included in fitting.
+        '''
+
+        #extract fit data with weights W
+        indices = np.arange(len(x))  #indices where W is nonzero (i.e. 1)
+
+        x_weighed = np.empty(len(indices))
+        target_weighed = np.empty(len(indices))
+        for i in range(len(indices)):
+        	x_weighed[i] = x[indices[i]]
+        	target_weighed[i] = target[indices[i]]
+
+
+    	#fit x = m * target + c to minimize res
+        A = np.vstack([target_weighed, np.ones(len(indices))]).T  #write x = m*target + c as x = A*(m, c)
+        m, c = np.linalg.lstsq(A, x_weighed)[0]
+        res = np.linalg.lstsq(A, x_weighed)[1]
+        res = res[0]
+
+        #determine fitness quality
+        indices1 = np.argwhere(target_weighed)  #all indices where target is nonzero
+        x0 = np.empty(0)  #list of values where x should be 0
+        x1 = np.empty(0)  #list of values where x should be 1
+        for i in range(len(target_weighed)):
+            if(i in indices1):
+                x1 = np.append(x1, x_weighed[i])
+            else:
+                x0 = np.append(x0, x_weighed[i])
+        if(min(x1) < max(x0)):
+            Q = 0
+        else:
+            Q = (min(x1) - max(x0)) / (max(x1) - min(x0) + abs(min(x0)))
+
+        F = self.fitnessparameters[0] * m / (res**(.5) + self.fitnessparameters[3] * abs(c)) + self.fitnessparameters[1] / res + self.fitnessparameters[2] * Q
+
+        return F
