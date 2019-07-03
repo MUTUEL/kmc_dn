ab29ba406ee7173d21e9c882ac95adb5c82a9908
Indrek Klanberg <indrkl@gmail.com>
2019-07-03 12:24:08 +0200

    bug fix and renamed sbatch.

--- examples/generate_samples.py
+++ examples/generate_samples.py
@@ -3,7 +3,7 @@ from voltage_search_tests import getRandomDn
 def main():
     dops = [30, 30, 30, 30, 20, 20, 20, 20, 10, 10, 10, 10]
 
-    for i in range(dops):
+    for i in range(len(dops)):
         dn = getRandomDn(dops[i], round(dops[i]/10.0))
         dn.saveSelf("sample%d.kmc"%(i+1))
 
